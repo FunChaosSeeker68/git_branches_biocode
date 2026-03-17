493175d18a55445734bac5b84bab6ea8b4c6dcd1
FunChaosSeeker68 <thebet5575@gmail.com>
2026-03-17 11:54:00 +0200

    added at precent

--- scripts/fasta_analysis.py
+++ scripts/fasta_analysis.py
@@ -8,3 +8,10 @@ def read_fasta(filename):
 
 sequence = read_fasta("data/sequence.fasta")
 print(sequence)
+AT=0
+sqw_num=len(sequence)
+for letter in sequence:
+    if letter == "A" or letter == "T":
+        AT=AT+1
+AT_PRECENT=(AT/sqw_num)*100
+print (AT_PRECENT)
